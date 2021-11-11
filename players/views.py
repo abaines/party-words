6667f94e47f6e84928d0dca9b8d2c282de950c8d
Alan Baines <abaines.vt+github@gmail.com>
2021-11-11 00:38:55 -0500

    create and use template

--- players/views.py
+++ players/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 from players.models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('players/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    output = template.render(context,request)
     return HttpResponse(output)
 
 def detail(request, player_id):
