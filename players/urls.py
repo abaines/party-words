b4dfdaa59a1ca093f23970267482b08de523081b
Alan Baines <abaines.vt+github@gmail.com>
2021-11-11 00:23:09 -0500

    rename argument
    question_id ➡ player_id

--- players/urls.py
+++ players/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:player_id>/', views.detail, name='detail'),
 
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:player_id>/results/', views.results, name='results'),
     
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:player_id>/vote/', views.vote, name='vote'),
 ]
